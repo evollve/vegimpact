1c8112e9015fe6ceffa849b70791141f01d490ad
evollve <adamcw@gmail.com>
2023-11-26 18:19:55 -0600

    Updates and fixes

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+function displayCircles(containerId, count, animalSymbol) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = ''; // Clear previous symbols
+    for (let i = 0; i < count; i++) {
+        const animal = document.createElement('span');
+        animal.className = 'animal-symbol';
+        animal.innerText = animalSymbol;
+        container.appendChild(animal);
+    }
+}
+
 function calculateImpact() {
     const yearsVegan = document.getElementById('years').value;
+    if (!yearsVegan || yearsVegan < 0 || !Number.isInteger(Number(yearsVegan))) {
+        alert('Please enter a valid number of years.');
+        return;
+    }
+
     const avgChickenWeight = 6 * 0.75; // 75% of 6 pounds
     const avgPigWeight = 250 * 0.74; // 74% of 250 pounds
     const avgCowWeight = 1300 * 0.62; // 62% of 1300 pounds
@@ -10,29 +26,21 @@ function calculateImpact() {
     const cowsPerYear = 58.8 / avgCowWeight;
     const fishPerYear = 20.5 / avgFishWeight;
 
-    displayCircles('chickenCircles', Math.round(yearsVegan * chickensPerYear));
-    displayCircles('pigCircles', Math.round(yearsVegan * pigsPerYear));
-    displayCircles('cowCircles', Math.round(yearsVegan * cowsPerYear));
-    displayCircles('fishCircles', Math.round(yearsVegan * fishPerYear));
+    // Update counts and display circles
+    updateAnimalCountAndDisplay('chicken', Math.round(yearsVegan * chickensPerYear), '🐔');
+    updateAnimalCountAndDisplay('pig', Math.round(yearsVegan * pigsPerYear), '🐖');
+    updateAnimalCountAndDisplay('cow', Math.round(yearsVegan * cowsPerYear), '🐄');
+    updateAnimalCountAndDisplay('fish', Math.round(yearsVegan * fishPerYear), '🐟');
 }
 
-function displayCircles(containerId, count) {
-    const container = document.getElementById(containerId);
-    container.innerHTML = ''; // Clear previous circles
-    for (let i = 0; i < count; i++) {
-        const circle = document.createElement('div');
-        circle.className = 'circle';
-        container.appendChild(circle);
-    }
+function updateAnimalCountAndDisplay(animal, count, symbol) {
+    document.getElementById(animal + 'Count').innerText = count;
+    document.getElementById(animal + 'Facts').style.display = 'block';
+    displayCircles(animal + 'Circles', count, symbol);
 }
 
-// Optional: Add color to circles based on the animal type
-function colorCircles(animalType, circle) {
-    switch(animalType) {
-        case 'chicken': circle.style.backgroundColor = 'gold'; break;
-        case 'pig': circle.style.backgroundColor = 'pink'; break;
-        case 'cow': circle.style.backgroundColor = 'skyblue'; break;
-        case 'fish': circle.style.backgroundColor = 'lightgreen'; break;
-        default: circle.style.backgroundColor = 'gray';
-    }
+function subscribe() {
+    const email = document.getElementById('email').value;
+    alert('Thank you for subscribing: ' + email);
+    // Integrate with backend or email service for actual functionality
 }
